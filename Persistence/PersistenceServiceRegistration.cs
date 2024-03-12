b5ffc885c48067e9dd9dece6cab3a912246d3d96
Umut Can YILMAZ <umut31can@gmail.com>
2024-03-12 11:30:38 +0300

    Persistence : Added Migrations

--- Persistence/PersistenceServiceRegistration.cs
+++ Persistence/PersistenceServiceRegistration.cs
@@ -18,7 +18,7 @@ public static class PersistenceServiceRegistration
     {
         //services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("nArchitecture"));
 
-        services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("RentACar"));
+        services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("RentACar")));
 
         services.AddScoped<IBrandRepository, BrandRepository>();
 
